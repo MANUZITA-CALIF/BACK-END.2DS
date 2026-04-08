ab72264fd6af9a8199eb63c5393ce5f41b745f82
germanimaria <manuela.calif@escola.pr.gov.br>
2026-04-08 11:17:07 -0300

    Update niquel.py

--- niquel.py
+++ niquel.py
@@ -2,9 +2,17 @@ import random
 import time
 import os
 
-SIMBOLOS = [ 'U0001F600' ]
+SIMBOLOS = [ '🚀', '⭐', '💎', '🪙', '🍀', '🎁' ]
 
 def limpar_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def exibir_maquina(colunas, status):
+    limpar_tela()
+    print('=' + 25 )
+    print("    TESTE    ")
+
+    print('=' + 25 )
+
+    print('=' + 25 )
+
