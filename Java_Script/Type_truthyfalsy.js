f8cb11daa5b4b824601caa7eb548f396f15bcb2d
Germani <germani.maria@escola.pr.gov.br>
2026-04-28 10:31:59 -0300

    Create Type_truthyfalsy.js

---
+++ Java_Script/Type_truthyfalsy.js
@@ -0,0 +1,24 @@
+const x = 0;
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+console.log(0 == false)
+console.log(" " == false)
+console.log(1 == true)
+
+console.log("null e undefinned")
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+console.log("Quando o undefined está em forma pura")
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
