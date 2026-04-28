a9f601b9b63fc2d694bb7f8da6dea17e51d9de26
Germani <germani.maria@escola.pr.gov.br>
2026-04-28 10:01:40 -0300

    Update Type_boolean.js

--- Java_Script/Type_boolean.js
+++ Java_Script/Type_boolean.js
@@ -2,4 +2,9 @@ const primeiroNumero = 5;
 const segundoNumero = 5;
 let cadastroAtivado = true;
 
-con
+//console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "Alura";
+
+console.log(texto1 === texto2)
