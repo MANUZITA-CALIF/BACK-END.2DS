cf23f027470b8f4d42d5d90b2f7f0c3c0822dc7f
Germani <germani.maria@escola.pr.gov.br>
2026-05-22 09:47:06 -0300

    Create array.js

---
+++ Java_Script/array.js
@@ -0,0 +1,17 @@
+const frutas = ['Maça', 'Banana', 'Laranja'];
+console.log(frutas.length);
+console.log(frutas);
+frutas.push('Manga');
+console.log(frutas);
+frutas.pop();
+console.log(frutas);
+
+const citricas = frutas.slice(0,2);
+console.log(citricas);
+
+frutas.slice(1,0, 'Kiwi');
+console.log(frutas);
+
+const verduras = ['Cenoura', 'Batata'];
+const frutaseverduras = frutas.concat(verduras);
+console.log(frutaseverduras);
